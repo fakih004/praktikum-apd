5a3fc5068ef92349c558ef16a2bade9a679331e7
fakih004 <faqihnabal@gmail.com>
2025-10-08 06:33:36 +0800

    posttest 4

---
+++ A1'25/pertemuan_4.py
@@ -0,0 +1,3 @@
+ulangi = 10
+for i in range(11):
+    print(f"perulangan ke -{i}")
---
+++ post-test/post-test-apd-2/A1'25/pertemuan-3/pertemuan_3.py
@@ -0,0 +1,38 @@
+# nilai = 70
+
+# status = "Lulus" if nilai >= 60 else "Tidak Lulus"
+# print(status)
+
+
+
+
+# nilai = 70
+
+# if nilai > 75:
+#     print('nilai A')
+# elif nilai > 65:
+#     print("nilai B")
+# else:
+#     print("nilai C")
+
+# suhu= 35
+
+# if suhu >= 35:
+#     print('PANAS')
+# if suhu > 30:
+#     print('sauna')
+# elif suhu >20:
+#     print('normal')
+# else:
+#     print('dingin')
+
+# tinggi = int(input('masukkan tinggi anda\t: '))
+
+# if tinggi >=145:
+#     print('bolrh masuk')
+# else:
+#     print('tidak b;eh masuk')
+
+# bisakah = 'silahkan masuk' if tinggi >= 145 else 'jangan masuk'
+# print(bisakah)
+
